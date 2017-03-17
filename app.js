f2d75153c644f67d451aabe049541c6eaec89123
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-03-17 20:03:32 +0800

    error handler

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const Koa = require('Koa')
+const logger = require('koa-logger')
+const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 const port = process.env.PORT || 2000
 
+app.use(logger())
+app.use(bodyParser())
+app.use(async (ctx, next) => {
+    ctx.hello = 6
+    console.log(1)
+    await next()
+    console.log(11)
+})
+
+app.use(async (ctx, next) => {
+    ctx.hello++
+    console.log(2)
+
+    if (ctx.hello > 5) {
+        ctx.throw('你找誰', 400)
+        return
+    } else {
+        await next()
+    }
+})
+
+app.use(async (ctx, next) => {
+    ctx.body = ctx.hello
+    console.log(3)
+    await next()
+    console.log(33)
+})
+
+// app.on('error', (err, ctx) => {
+//     ctx.throw(err.message || 'Uknown Error', err.status || 500)
+// })
 
 const server = app.listen(port)
-server.on('listeneing', () => console.log('listening on port ' + port))
+server.on('listening', () => console.log('listening on port ' + port))
